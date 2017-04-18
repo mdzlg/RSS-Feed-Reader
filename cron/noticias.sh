221a722dd3d129a5a2906464efc2ed07c22dfa0b
JBlanco <jblanco@lagaceta.com.ar>
2017-04-18 14:14:47 +0000

    Puesta al día

--- cron/noticias.sh
+++ cron/noticias.sh
@@ -5,4 +5,5 @@ api_key='d1ufn2m6kmtwzt6iw9symnlfycs0d4nfkcybdxmv'
 #rss_url='http://feeds.feedburner.com/TitularesLaGaceta'
 rss_url="http://www.lagaceta.com.ar/rss/newsletter/online"
 url_base='http://www.lagaceta.com.ar/'
-curl -s -o $html_dir/noticias.json "https://api.rss2json.com/v1/api.json?api_key=$api_key&rss_url=$rss_url"
+#curl -s -o $html_dir/noticias.json "https://api.rss2json.com/v1/api.json?api_key=$api_key&rss_url=$rss_url"
+echo "https://api.rss2json.com/v1/api.json?api_key=$api_key&rss_url=$rss_url"
